b243b3eeadb6a7de5c6550510404f69007cd3657
unknown <sakibsafat47@gmail.com>
2024-10-12 21:15:56 +0600

    header and footer implemnt on dashboard

--- src/app/restaurant/dashboard/page.js
+++ src/app/restaurant/dashboard/page.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import RestaurantFooter from "@/app/_components/RestaurantFooter";
+import RestaurantHeader from "@/app/_components/RestaurantHeader";
+import React from "react";
 
 const Dashboard = () => {
-    return (
-        <div className='min-h-screen'>
-            <h3 className="text-3xl text-slate-900 text-center">
-            Restaurant Sign Up
-            </h3>
-        </div>
-    );
+  return (
+    <>
+      <RestaurantHeader />
+      <div className="min-h-screen">
+        <h3 className="text-3xl text-slate-900 text-center">
+          Restaurant Sign Up
+        </h3>
+      </div>
+      <RestaurantFooter />
+    </>
+  );
 };
 
 export default Dashboard;
