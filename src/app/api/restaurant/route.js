7d2e72e4d5744790a8efca74a4b97ce7a66e6f1a
unknown <sakibsafat47@gmail.com>
2024-10-14 23:46:32 +0600

    log in route for backend

--- src/app/api/restaurant/route.js
+++ src/app/api/restaurant/route.js
@@ -5,45 +5,46 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    
     if (mongoose.connection.readyState === 0) {
       await mongoose.connect(connectionStr);
     }
 
-   
     const data = await restaurantSchema.find();
 
-    
     return NextResponse.json({ result: true, data });
-
   } catch (error) {
-   
     console.error("Error fetching data:", error);
-    return NextResponse.json({ result: false, error: "Failed to fetch data" }, { status: 500 });
+    return NextResponse.json(
+      { result: false, error: "Failed to fetch data" },
+      { status: 500 }
+    );
   }
 }
 
 export async function POST(request) {
-    try {
-      
-      const payload = await request.json();
-  
-      
-      if (mongoose.connection.readyState === 0) {
-        await mongoose.connect(connectionStr);
-      }
-  
-      
-      let restaurant = new restaurantSchema(payload);
-  
-      
-      const result = await restaurant.save();
-  
-      
-      return NextResponse.json({ result, success: true });
-    } catch (error) {
-      
-      console.error("Error saving restaurant:", error);
-      return NextResponse.json({ success: false, error: error.message }, { status: 500 });
+  try {
+    const payload = await request.json();
+    let result;
+    if (mongoose.connection.readyState === 0) {
+      await mongoose.connect(connectionStr);
     }
+    if (payload.login) {
+      result = await restaurantSchema.findOne({
+        email: payload.email,
+        password: payload.password,
+      });
+    } else {
+      let restaurant = new restaurantSchema(payload);
+
+      result = await restaurant.save();
+    }
+
+    return NextResponse.json({ result, success: true });
+  } catch (error) {
+    console.error("Error saving restaurant:", error);
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 500 }
+    );
   }
+}
